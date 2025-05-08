bb55d6eb39fcdcd499ef37690ad8a5e42cf50090
akuahamda <hamdalamohammed80@gmail.com>
2025-05-08 16:34:26 -0700

    assignment

--- index.js
+++ index.js
@@ -16,3 +16,35 @@
 
     });
 })();
+
+let btnClicked = () => {
+    let btn = document.getElementById('button');
+    btn.addEventListener('click', () => {
+      document.body.classList.toggle('bg-white');
+      document.body.classList.toggle('text-black');
+      document.body.classList.toggle('bg-black');
+      document.body.classList.toggle('text-white');
+    });
+  };
+  btnClicked();
+  
+  let changeTime = ()=>{
+    let btnChange = document.getElementById('date');
+    btnChange.addEventListener('click',()=>{
+        let now = new Date();
+      let h4Element = document.createElement('h4');
+      h4Element.textContent = now;
+      document.body.appendChild(h4Element);
+      h4Element.style.margin = '10px'
+    });
+  };
+  changeTime();
+
+  let colorPicker = ()=>{
+    let inputType = document.querySelector('input');
+    let bottonElement = document.getElementById('colorpicker');
+    bottonElement.addEventListener('click',()=>{
+      document.body.style.backgroundColor = inputType.value;  
+    });
+  };
+  colorPicker();
